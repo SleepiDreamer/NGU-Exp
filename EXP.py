39f646ae4700a5f35fd8f8c225482d0370d3e179
bliksemremi <46381050+bliksemremi@users.noreply.github.com>
2019-11-05 14:28:32 +0100

    Update EXP.py

--- EXP.py
+++ EXP.py
@@ -15,7 +15,7 @@ barsExp = recbars - bars
 
 if recpow != 0 and recbars != 0:
     print()
-    print("You need to have " + str(powerExp) + " more speed, and " + str(barsExp) + " more bars")
+    print(f"You need to have {powerExp} more power, and {barsExp} more bars")
     print()
     print("EXP needed for full efficiency:")
     print("power: " + str(math.ceil(powerExp * 150)) + " EXP")
@@ -24,5 +24,4 @@ if recpow != 0 and recbars != 0:
 else:
     print("You are fully efficient!")
 
-while True:
-    pass
+input()
